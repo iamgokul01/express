191bd418c804b53a3422cb58c358b999609e43bf
iamgokul01 <gokuldonasekar@gmail.com>
2023-09-06 14:49:42 +0000

    converted snakecase to camelcase

--- app.js
+++ app.js
@@ -26,24 +26,35 @@ initDBAndServer();
 
 app.get("/players/", async (request, response) => {
   let fetchPlayers = `select * from cricket_team`;
-  let result = await db.all(fetchPlayers);
-  response.send(result);
+  let dbResponse = await db.all(fetchPlayers);
+  let resultObj = [];
+  for (let obj of dbResponse) {
+    let camelObj = {
+      playerId: obj.player_id,
+      playerName: obj.player_name,
+      jerseyNumber: obj.jersey_number,
+      role: obj.role,
+    };
+    resultObj.push(camelObj);
+  }
+  console.log(resultObj);
+  response.send(resultObj);
 });
 
 app.post("/players/", async (request, response) => {
   const playerData = request.body;
 
   let { playerName, jerseyNumber, role } = playerData;
-
+  console.log(playerData);
   const addPlayerQuery = `
   insert into cricket_team (player_name,jersey_number,role)
   values(
-      '${playerName}','${jerseyNumber}','${role}'
+      '${playerName}',${jerseyNumber},'${role}'
   );`;
   const dbResponse = await db.run(addPlayerQuery);
   const bookId = dbResponse.lastId;
-  console.log(playerData);
-  response.send("Player Added to the team");
+
+  response.send("Player Added to Team");
 });
 
 app.get("/players/:playerId", async (request, response) => {
@@ -51,8 +62,14 @@ app.get("/players/:playerId", async (request, response) => {
   const fetchPlayer = `
   select * from cricket_team where player_id = ${playerId};`;
   let dbResponse = await db.get(fetchPlayer);
-  response.send(dbResponse);
-  console.log(dbResponse);
+  let result = {
+    playerId: dbResponse.player_id,
+    playerName: dbResponse.player_name,
+    jerseyNumber: dbResponse.jersey_number,
+    role: dbResponse.role,
+  };
+  response.send(result);
+  console.log(result);
 });
 
 app.put("/players/:playerId", async (request, response) => {
@@ -63,11 +80,9 @@ app.put("/players/:playerId", async (request, response) => {
   const updatePlayer = `
   update cricket_team
   set player_name = '${playerName}',
-  jersey_number = '${jerseyNumber}',
+  jersey_number = ${jerseyNumber},
   role = '${role}'
-  where player_id = ${playerId}
-
-  `;
+  where player_id = ${playerId};`;
   let dbResponse = await db.run(updatePlayer);
   response.send("Player Details Updated");
   console.log(dbResponse);
